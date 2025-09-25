836db0dc776d48c29b3fbc49355033397c25f3ac
Jonathan <jesusetia17@gmail.com>
2025-09-25 15:34:35 +0800

    hw2

---
+++ hw2.py
@@ -0,0 +1,11 @@
+import cmath
+
+def root2(a, b, c):
+    D = b*b - 4*a*c
+    r1 = (-b + cmath.sqrt(D)) / (2*a)
+    r2 = (-b - cmath.sqrt(D)) / (2*a)
+    return (r1, r2)
+
+print(root2(1, -5, 6))
+print(root2(1, 4, 3))
+print(root2(1, 1, 1))
